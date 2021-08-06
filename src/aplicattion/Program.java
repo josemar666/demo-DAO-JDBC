26130b6090b1772d8efd65436931eaa94125d612
Josemar666 <josemarlob1@hotmail.com>
2021-08-06 20:18:38 -0300

    Department entity class

---
+++ src/aplicattion/Program.java
@@ -0,0 +1,13 @@
+package aplicattion;
+
+import model.entites.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Department cu = new Department(1,"experts");
+		System.out.println(cu);
+
+	}
+
+}

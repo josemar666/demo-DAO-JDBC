08fcf728bff0247872e2e1323b5c5f688a460919
Josemar666 <josemarlob1@hotmail.com>
2021-08-06 20:36:17 -0300

    Seller entity class

--- src/aplicattion/Program.java
+++ src/aplicattion/Program.java
@@ -1,13 +1,18 @@
 package aplicattion;
 
+import java.util.Date;
+
 import model.entites.Department;
+import model.entites.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		Department cu = new Department(1,"experts");
 		System.out.println(cu);
-
+		
+		Seller pica = new Seller(21,"bob" ,"bobo@gmail.com",new Date(),3000.00 , cu);
+       System.out.println(pica);
 	}
 
 }

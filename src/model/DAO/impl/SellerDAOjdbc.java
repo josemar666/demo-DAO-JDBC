48f1308ee304d2f1898affa06091db3960b18a7e
Josemar666 <josemarlob1@hotmail.com>
2021-08-09 20:45:37 -0300

    SellerDaoJDBC and DaoFactory

---
+++ src/model/DAO/impl/SellerDAOjdbc.java
@@ -0,0 +1,40 @@
+package model.DAO.impl;
+
+import java.util.List;
+
+import model.DAO.SellerDao;
+import model.entites.Seller;
+
+public class SellerDAOjdbc implements SellerDao {
+
+	@Override
+	public void insert(Seller obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Seller obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteByid(Integer id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Seller findByid(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Seller> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
